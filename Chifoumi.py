fe43e7fe96ce222d88bf36cd84092adbe60349ee
Maxime BIDEAU <m.bideau@workstation19.nsi.lan>
2024-10-11 13:25:09 +0200

    Version 1

--- Chifoumi.py
+++ Chifoumi.py
@@ -11,29 +11,32 @@ def choix_joueur():
 
 def verdict(choix_joueur, choix_ordi):
     if choix_joueur==choix_ordi:
+        print("Egalité")
         return 0
     if choix_joueur=='p' and choix_ordi=='c' or choix_joueur=='f' and choix_ordi=='p' or choix_joueur=='c' and choix_ordi=='f':
+        print("Gagné")
         return 1
     if choix_joueur=='p' and choix_ordi=='f' or choix_joueur=='f' and choix_ordi=='c' or choix_joueur=='c' and choix_ordi=='p':
+        print ("Perdu")
         return -1
     else:
         return 2
 
 
 def partie():
-    joueur=choix_joueur()
+    joueur=input("Tape \"p\" pour pierre, \"f\" pour feuille ou \"c\" pour ciseau. ")
     ordi=choix_ordi()
-    resultat=verdict(joueur, ordi)
-    print (f"Choix de l'ordinateur : {ordi}")
-    if resultat==0:
-        print("Egalité")
-    if resultat==-1:
-        print ("Perdu")
-    if resultat==1:
-        print("Gagné")
-    return resultat
+    print (f"Choix ordi : {ordi}")
+    return verdict(joueur, ordi)
 
 def jeu_pfc():
+    conteur = 0
     while verdict(choix_joueur(),choix_ordi())!=2:
-        conteur=conteur+
+        conteur=conteur+partie()
+        print (f"Nombre de points : {conteur}")
+    print ("Fin du jeu")
+    print (f"Score total : {conteur}")
 
+
+
+jeu_pfc()
